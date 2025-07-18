a2e22f6111445733e3faab20a4cbf36384a60ed5
Aeliyahaider <aeliyahaider933@gmail.com>
2025-07-18 09:41:48 +0500

    HTML, CSS AND JAVASCRIPT

---
+++ start.js
@@ -0,0 +1,16 @@
+console.log("AELIYA HAIDER FIRST LINE OF CODE JS");
+console.log("HI I'M AELIYA HAIDER SUTO");
+console.log("HELLO"); // this line of code is use to print message
+fullname = "AELIYA HAIDER SUTO";
+console.log(fullname);
+age = 20;
+console.log(age);
+value = null;
+console.log(value);
+undef = undefined;
+console.log(undef);
+isfollow = true;
+console.log(isfollow);
+iffollow = false;
+console.log(iffollow);
+alert("ALA POCHI INSAN LOKA SITH LA");
